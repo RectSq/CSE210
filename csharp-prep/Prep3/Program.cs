8d8241f45a8f53239484424d98a344b255630110
RectSq <rectsq@byui.edu>
2026-01-10 16:04:46 -0700

    Made prep 3. uses do while as this is the only case they are useful in...

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random numberGenerator = new Random();
+        string play;
+        int guess;
+        do{
+        int magicNumber = numberGenerator.Next(1,101);
+        
+        do
+        {
+            Console.Write("Guess the Number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("\nTry guessing a bit lower.");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("\nTry guessing a bit higher.");
+            }
+
+        } while(guess!=magicNumber);
+        Console.WriteLine("\n Congratulations You Won!");
+        Console.Write("\nDo you want to play again? (y/n) ");
+        play = Console.ReadLine();
+        } while(play == "y"||play == "yes");
     }
 }
