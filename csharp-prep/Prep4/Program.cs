e2c26910318f60b08aed12229dd578462232ae22
RectSq <rectsq@byui.edu>
2026-01-10 16:20:51 -0700

    Added min because it takes second but I do have other work due today.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int max = 0;
+        int min = 0;
+        float average;
+
+        //do loop
+        Console.WriteLine("Enter a list of numbers, type 0 to finished.");
+        do
+        {
+            Console.Write("Enter a Number: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+
+        } while(numbers[numbers.Count-1]!=0);
+
+        //calc
+        foreach(int num in numbers)
+        {
+            sum += num;
+            max = Math.Max(max,num);
+            min = Math.Min(min,num);
+
+        }
+        average = (float)sum/(numbers.Count-1);
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Largest: {max}");
+        Console.WriteLine($"Largest: {min}");
     }
 }
