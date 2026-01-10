71ec4358df13276d44bc5c1a2f7ce7ca601d59f0
RectSq <rectsq@byui.edu>
2026-01-10 15:48:42 -0700

    Prep 2 with letter sign didn't notice it before quiz lol.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade = "";
+        int gradeDangle = 0;
+        string letterGradeSign = "";
+
+        if(grade >= 90){letterGrade = "A";}
+        else if(grade >= 80){letterGrade = "B";}
+        else if(grade >= 70){letterGrade = "C";}
+        else if(grade >= 60){letterGrade = "D";}
+        else if(grade < 60){letterGrade = "F";}
+
+        
+        gradeDangle = grade%10;
+        if (gradeDangle >= 7&&grade<90&&grade>60)
+        {
+            letterGradeSign = "+";
+        }
+        else if (gradeDangle <= 3&&grade>60)
+        {
+            letterGradeSign = "-";
+        }
+        
+        Console.WriteLine($"\n{letterGrade}{letterGradeSign}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Come on. Give it another shot!");
+        }
+
     }
 }
