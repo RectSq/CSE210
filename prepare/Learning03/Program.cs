bffd0512b88626d756757fdea699208550f303d4
RectSq <rectsq@byui.edu>
2026-01-21 21:49:17 -0700

    Made fraction with all the functions.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,66 @@ using System;
 
 class Program
 {
+    public class Fraction
+    {
+        private int _top;
+        private int _bottom;
+
+        public Fraction()
+        {
+            _top = 42;
+            _bottom = 2;
+        }
+        public Fraction(int wholeNumber)
+        {
+            _top = wholeNumber;
+            _bottom = 1;
+        }
+        public Fraction(int top, int bottom)
+        {
+            _top = top;
+            SetBottom(bottom);
+        }
+
+        public int GetTop()
+        {
+            return _top;
+        }
+        public void SetTop(int top)
+        {
+            _top = top;
+        }
+        public int GetBottom()
+        {
+            return _bottom;
+        }
+        public void SetBottom(int bottom)
+        {
+            if(bottom!=0){
+                _bottom = bottom;
+            }
+            else{
+                Console.WriteLine("Bottom value cannot be set to 0\nbottom set to 1");
+                _bottom = 1;
+            }
+        }
+        public string GetFractionString()
+        {
+            string output  = $"{_top}/{_bottom}";
+            return output;
+        }
+        public double GetDecimalValue()
+        {
+            return (double)_top / (double)_bottom;
+        }
+
+
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction(4,2);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+
+
+    private String hiddenString = "And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters.";
+
+    public class hiddenPhrase {
+        private string scripture;
+        public string Scripture
+        {
+            get {return scripture;}
+            set {scripture = value;}
+        }
+        public void displayWord()
+        {
+            Console.Clear();
+            Console.WriteLine("abab");
+
+        }
+
+
+    }
+
+    
+    hiddenPhrase a = new hiddenPhrase();
+    int start = 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        while(true){
+
+        }
+
+
+
     }
 }
