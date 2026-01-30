35a28215595b44dcbd98736de20159a958edd30c
RectSq <rectsq@byui.edu>
2026-01-29 23:40:02 -0700

    Harah today.
    Finished learning04.
    
    Added classes as individual files and added test cases

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment = new MathAssignment("Roberto Rodriguez","Fractions","3.7","8-19");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetHomeWorkList());
+
+
+        WritingAssignment assignment2 = new WritingAssignment("Ferry Hampton","Classical Literature","The many unfortunate accounts of Robert Longsdale");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }
