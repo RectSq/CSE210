c242e48f31f04cd493bd3ec58284f0d795123a3c
RectSq <rectsq@byui.edu>
2026-02-03 22:04:31 -0700

    Created program and met requirements

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Build and propagate list
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 10));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
